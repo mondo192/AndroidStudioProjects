b9156d54d3dc55d0d0f5cf5bdf77c673f4c31fe8
eamonn-keogh <31599023+eamonn-keogh@users.noreply.github.com>
2018-08-08 23:46:37 +0100

    Completed basic toast UI

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.hellotoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
