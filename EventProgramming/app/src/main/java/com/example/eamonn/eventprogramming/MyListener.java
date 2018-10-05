129766e20feceea4c01a696fbdf55b6c41f35552
mondo192 <31599023+mondo192@users.noreply.github.com>
2018-10-05 22:20:14 +0100

    Started lab4

--- EventProgramming/app/src/main/java/com/example/eamonn/eventprogramming/MyListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.eamonn.eventprogramming;
-
-import android.view.View;
-
-public interface MyListener {
-    public void callback(View view, String result);
-}
---
+++ Lab4/app/src/main/java/com/example/eamonn/lab4/GreenFlashlightActivity.java
@@ -0,0 +1,5 @@
+package com.example.eamonn.lab4;
+
+public class GreenFlashlightActivity {
+
+}
