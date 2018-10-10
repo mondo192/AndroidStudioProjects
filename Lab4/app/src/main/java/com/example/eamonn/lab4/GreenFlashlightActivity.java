0a4aecc919308c35d70dd201350ac9d7b15755bb
mondo192 <31599023+mondo192@users.noreply.github.com>
2018-10-10 10:48:51 +0100

    Lab4 Done

--- Lab4/app/src/main/java/com/example/eamonn/lab4/GreenFlashlightActivity.java
+++ Lab4/app/src/main/java/com/example/eamonn/lab4/GreenFlashlightActivity.java
@@ -1,5 +1,26 @@
 package com.example.eamonn.lab4;
 
-public class GreenFlashlightActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+public class GreenFlashlightActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_greenflashlight);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(),  MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
