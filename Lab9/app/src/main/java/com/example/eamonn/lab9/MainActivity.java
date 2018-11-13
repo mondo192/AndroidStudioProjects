ede921468cb57bd2f57f6d846acff75bdd4cc429
mondo192 <31599023+mondo192@users.noreply.github.com>
2018-11-13 21:29:16 +0000

    Working on lab 9

---
+++ Lab9/app/src/main/java/com/example/eamonn/lab9/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.eamonn.lab9;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private TextView locationText;
+    private LocationManager locationManager;
+    private long minTime = 500;
+    private float minDistance = 1;
+    private static final int MY_PERMISSION_GPS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationText = findViewById(R.id.location);
+        setUpLocation();
+    }
+
+    private void setUpLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Check if there is permission for location tracking already stored
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_GPS);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance,
+                    this);
+        }
+    }
+
+    public void onLocationChanged(Location location) {
+        String latestLocation = "";
+
+        if (location != null) {
+            latestLocation = String.format("Current Location: Latitude %1$s Longitude: %2$s",
+                    Math.round(location.getLatitude()), Math.round(location.getLongitude()));
+        }
+        locationText.setText("GPS Location" + "\n" + latestLocation);
+    }
+
+    public void onProviderDisabled(String provider) { }
+
+    public void onProviderEnabled(String provider) { }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_GPS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected  void onPause() {
+        super.onPause();
+    }
+
+}
